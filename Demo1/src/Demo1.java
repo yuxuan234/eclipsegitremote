e3afee4113ff024f4c71a5eb81d39cc8accd9f73
yuxuan234 <yuxuan236@yeah.net>
2019-05-18 12:07:20 +0800

    发布项目

---
+++ Demo1/src/Demo1.java
@@ -0,0 +1,43 @@
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
+import org.junit.Test;
+
+public class Demo1 {
+
+	@Test
+	public void testMkDir() throws Exception {
+
+		Configuration conf = new Configuration();
+		conf.set("fs.defaultFS", "hdfs://192.168.40.128:9000");
+
+		FileSystem client = FileSystem.get(conf);
+
+		client.mkdirs(new Path("/temp"));
+
+		client.close();
+	}
+
+	@Test
+	public void testUpload() throws Exception {
+		Configuration conf = new Configuration();
+		conf.set("fs.defaultFS", "hdfs://192.168.40.128:9000");
+
+		FileSystem client = FileSystem.get(conf);
+
+		InputStream in = new FileInputStream("C:\\software\\Eclipse\\data.txt");
+
+		OutputStream out = client.create(new Path("/temp/data.txt"));
+
+		IOUtils.copyBytes(in, out, 1024);
+
+		client.close();
+	}
+
+}
